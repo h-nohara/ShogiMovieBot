43a201f6a6486fd9bca8996fd044de6628f7427d
Nohara <selaphymsigoto@gmail.com>
2018-11-18 19:00:04 +0900

    register movie records

--- board/src/movie/generate_movies.py
+++ board/src/movie/generate_movies.py
@@ -1,4 +1,4 @@
-import os, sys, json, shutil, pickle
+import os, sys, json, shutil, pickle, glob2
 from django.shortcuts import render, redirect
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -14,6 +14,7 @@ from board.models.movie import Movie
 from board.src.load_save.modify import modify
 from board.src.generate_movie.make_movie.history_to_movie import history_to_movies
 from accounts.src.utils import generate_basename, movie_path_local, pickle_path_local
+from board.src.google_drive.test import upload_movie
 
 
 @csrf_exempt
@@ -53,10 +54,28 @@ def generate_movies(request):
     history_to_movies(history, temporal_image_dir, temporal_movie_dir)
 
     print("success : generate movies")
+    shutil.rmtree(temporal_image_dir)
 
-    # todo
+    # Movieレコードを削除
+    record_list_Movie = Movie.objects.filter(project=record_Project)
+    for record_Movie in record_list_Movie:
+        record_Movie.delete()
+
+    ###### クラウドの動画を削除
+
     # 動画のpathを決め、Movieレコードとして保存
-    # クラウドのストレージの登録
-    # 動画のアップロード
+    ext = "mp4"
+    movies = glob2.glob(os.path.abspath(os.path.join(temporal_movie_dir, "movie_*.mp4")))
+    for i in range(len(movies)):
+        movie_basename = generate_basename(key+str(i), ext)
+        # 動画をアップロード
+        print("start upload movie [")
+        upload_movie(movies[i], movie_basename)
+        print("finish upload movie ]")
+        record_Movie = Movie(
+            project = record_Project,
+            basename = movie_basename
+        )
+        record_Movie.save()
 
     return JsonResponse({"code" : 200})
