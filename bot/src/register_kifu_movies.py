0d4340dbaf9d2f2a54162d6e2a30610e33997129
Nohara <selaphymsigoto@gmail.com>
2018-12-18 19:50:53 +0900

    upload and register kifu movie

---
+++ bot/src/register_kifu_movies.py
@@ -0,0 +1,37 @@
+import os, sys, glob2
+from ShogiMovieBot.settings import BASE_DIR
+
+# api
+from accounts.src.utils.generate_fname import generate_basename
+from accounts.src.utils import bucket, fname_cloud, upload_file
+
+
+# db
+from bot.models.kifu_movie import KifuMovie
+
+
+def register_kifu_movies(dirname):
+
+    movies = glob2.glob(os.path.join(dirname, "*.mp4"))
+    print(movies)
+
+    for i, movie in enumerate(movies):
+
+        print(i)
+        print(movie)
+
+        key = generate_basename("kifumovie{}".format(i), "mp4")
+
+        # アップロード
+        upload_file(bucket, movie, key)
+
+        # dbに登録
+        record_KifuMovie = KifuMovie(path=fname_cloud(key), opening="四間飛車")
+        record_KifuMovie.save()
+
+
+if __name__ == "__main__":
+
+    dirname = os.path.join(BASE_DIR, "bot/static/kifu_movies/四間飛車_packed_up")
+    register_kifu_movies(dirname)
+    pritn("all finished")
