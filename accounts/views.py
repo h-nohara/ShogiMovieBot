5a47639ac7a4fe63a8415b2d4f08eacd46e74e48
Nohara <selaphymsigoto@gmail.com>
2019-01-18 10:01:52 +0900

    login_required

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 
 def login_page(request):
     return render(request, "accounts/login.html")
 
+@login_required(login_url="/account/login")
 def home_page(request):
     return render(request, "accounts/home.html")
 
--- bot/views.py
+++ bot/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url="/account/login")
 def project_scenarios_page(request):
     return render(request, "bot/project_scenarios.html")
 
+@login_required(login_url="/account/login")
 def scenario_editor_page(request):
     return render(request, "bot/scenario_editor.html")
