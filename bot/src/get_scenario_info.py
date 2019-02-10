fa3395aad0a6434d9d1e6228c3a326c850818f3f
Nohara <selaphymsigoto@gmail.com>
2019-02-11 00:44:15 +0900

    mod

--- bot/src/get_scenario_info.py
+++ bot/src/get_scenario_info.py
@@ -44,7 +44,7 @@ def get_scenario_info(scenario_id):
     messages = get_messages(scenario_id)
 
     author = record_Scenario.project.user
-    record_list_Subscription = Subscription.objects.filter(reader=author, author=author)
+    record_list_Subscription = Subscription.objects.filter(reader=author, author=author, is_enabled=True)
     if len(record_list_Subscription) > 0:
         is_subsc = True
     else:
