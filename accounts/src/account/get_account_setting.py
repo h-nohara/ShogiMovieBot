0598a3d2e9e01ba8e04ecf16b522229d8411636d
Nohara <selaphymsigoto@gmail.com>
2019-02-19 00:11:59 +0900

    user setting

--- accounts/src/account/get_account_setting.py
+++ accounts/src/account/get_account_setting.py
@@ -1,5 +1,6 @@
 import os, sys
 import json
+import datetime, pytz
 
 from django.contrib.auth.decorators import login_required
 from django.http.response import JsonResponse
@@ -8,7 +9,6 @@ from django.views.decorators.csrf import csrf_exempt
 
 # db
 from accounts.models.user import User
-
 
 
 @login_required
@@ -23,13 +23,18 @@ def get_account_setting_request(request):
     interval_RandomSubscription = record_User.interval_RandomSubscription
     next_date_RandomSubscription =record_User.next_date_RandomSubscription
 
+    # 次回配信日を文字列に
+    jp = pytz.timezone("Asia/Tokyo")
+    next_date_RandomSubscription = next_date_RandomSubscription.astimezone(jp)
+    str_next_date_RandomSubscription = "{0:%Y-%m-%d}".format(next_date_RandomSubscription)
+
     data = {
         "username" : username,
         "nickname" : nickname,
         "is_enabled_RandomSubscription_own_scenario" : is_enabled_RandomSubscription_own_scenario,
         "is_enabled_RandomSubscription_others_scenario" : is_enabled_RandomSubscription_others_scenario,
         "interval_RandomSubscription" : interval_RandomSubscription,
-        "next_date_RandomSubscription" : next_date_RandomSubscription
+        "next_date_RandomSubscription" : str_next_date_RandomSubscription
     }
 
     result = {
