027cbbfcaa3e09d45a56327cc3cdfb2ee0f13d49
Nohara <selaphymsigoto@gmail.com>
2019-02-24 02:34:21 +0900

    change to new

--- board/views.py
+++ board/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url="/account/login")
 def board_page(request):
-    return render(request, "board/gui.html")
+    return render(request, "board/board_editor.html")
 
 @login_required(login_url="/account/login")
 def initial_board_page(request):
