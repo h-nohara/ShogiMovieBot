39da0b97fef7fe228f1a78f869d9d48745816ff2
Nohara <selaphymsigoto@gmail.com>
2019-03-03 01:23:51 +0900

    nonButton

---
+++ board/static/js/ShogiBoard/history.js
@@ -0,0 +1,499 @@
+
+
+function update_d3(dataset){
+
+    d3.select("#history_scroll").selectAll(".OneAction").remove();
+    d3.select("#sub_scenarios").selectAll(".sub_sc").remove();
+
+    // console.log("dataset");
+    // console.log(dataset);
+
+
+    d3.select("#history_scroll").selectAll(".OneAction")
+
+    .data(dataset)
+    .enter()
+    .append("div")
+    .append("button")
+    .attr("class", "OneAction")
+
+    // ボタンのテキスト
+    .text(function(action){
+        let keys = Object.keys(action);
+        if (keys.indexOf("move") >= 0){
+            return "(" + String(action["order_of_hand"]) + ")  " + action["move_str"];
+        }
+        else if (keys.indexOf("message") >= 0){
+            // return "テロップ";
+            let m = action["message"];
+            // テキストがあったら
+            if ( Object.keys(m).indexOf("text") >= 0) {
+                return m["text"].slice(0,8);
+            }
+            else {
+                return "メッセージ";
+            }
+        }
+        else if (keys.indexOf("scenarios") >= 0){
+            return "分岐点";
+        }
+        else if (keys.indexOf("fly_to") >= 0){
+            return "fly_to";
+        }
+        else {
+            return "other";
+        }
+    })
+
+    // ボタンの色の決定と、現在見ているボタンのチェック
+    .style("background-color", function(action){
+
+        if (Object.keys(action).indexOf("scenarios") >= 0){
+            // is_watchingのチェック
+            if (action["is_watching"] === true){
+                History.watching_dom = this;
+
+                // if (draw_board){
+                //     SBoard.Board = deepcopy_Board(action["board_state"]);
+                //     SBoard.draw_main_board();
+                // }
+            }
+            return "sandybrown";
+        }
+        else{
+            // is_watchingのチェック
+            if (action["is_watching"] === true){
+                History.watching_dom = this;
+
+                // if (draw_board){
+                //     SBoard.Board = deepcopy_Board(action["board_state"]);
+                //     SBoard.draw_main_board();
+                // }
+
+                return "cornflowerblue";
+            }
+            else{
+                return "white";
+            }
+        }
+
+        // else if(Object.keys(action).indexOf("is_watching") >= 0){
+
+        //     // どのボタンを見ているか、ここでチェック
+        //     if (action["is_watching"] === true){
+        //         History.watching_dom = this;
+        //         return "cornflowerblue";
+        //     }
+        //     else{
+        //         return "white";
+        //     }
+        // }
+    })
+
+    // ボタンがクリックされたら
+    .on("click", function(action){
+
+        // 将棋盤の状態を元に戻す
+        SBoard.init_state_now();
+
+        // ボタンの背景色を更新、分岐点以外のボタンなら分岐一覧を非表示に
+
+        if ($(History.watching_dom).text() == "分岐点"){
+            $(History.watching_dom).css("background-color", "sandybrown");
+            $("#sub_scenarios").html("");
+        }
+        else{
+            $(History.watching_dom).css("background-color", "white");
+        }
+
+        // 現在見ているボタンを更新
+        History.watching_action["is_watching"] = false;
+        History.watching_action = action;
+        History.watching_dom = this;
+        $(this).css("background-color", "cornflowerblue");
+
+
+        // シナリオ分岐ボタンを押したら
+        if (Object.keys(action).indexOf("scenarios") >= 0){
+
+            $(this).css("background-color", "sandybrown");
+
+            d3.select("#sub_scenarios")
+            .selectAll(".sub_sc")
+            .data(action["scenarios"])
+            .enter()
+            .append("div")
+            .append("button")
+            .attr("class", "sub_sc")
+            // シナリオ選択ボタンのテキスト
+            .text(function(action_list){
+                let first_action = action_list[0];
+                if ( Object.keys(first_action).indexOf("move") >= 0 ){return first_action["move_str"];}
+                else if ( Object.keys(first_action).indexOf("message") >= 0 ){
+                    let m = first_action["message"];
+                    // テキストがあったら
+                    if ( Object.keys(m).indexOf("text") >= 0) {
+                        return m["text"].slice(0,8);
+                    }
+                    else {
+                        return "メッセージ";
+                    }
+                }
+            })
+            // シナリオ選択ボタンがクリックされたら
+            .on("click", function(sub_sc, i_sub_sc){
+
+                action["selected_scenario"] = i_sub_sc;
+                History.watching_action["is_watching"] = false;
+                action["is_watching"] = true;
+
+                var emp_list = [];
+                list_showing_actions(History.history, emp_list);
+                update_d3(emp_list);
+            });
+
+        }
+
+        // それ以外のボタンを押したら
+        else{
+            
+            // メッセージの各項目を代入
+            show_message_contents(action);
+
+            // parentを表示
+            // console.log("parent");
+            // console.log(action["parent"]);
+        }
+
+        
+        // 盤面をその時の状態に
+        SBoard.Board = deepcopy_Board(action["board_state"]);
+        SBoard.draw_main_board();
+        
+    });
+
+    // 現在選択されている箇所へスクロール
+    auto_scroll_history();
+    // let scroll_box_height = $("#history_scroll").height();
+    // let pos = $(History.watching_dom).position().top;
+    // if (pos > scroll_box_height){
+    //     $("#history_scroll").scrollTop(pos-10);
+    // }
+
+}
+
+function auto_scroll_history(){
+    let scroll_box_height = $("#history_scroll").height();
+    let pos = $(History.watching_dom).position().top;
+    if (pos > scroll_box_height){
+        $("#history_scroll").scrollTop(pos-10);
+    }
+}
+
+var emp_list = [];
+
+
+function list_showing_actions(history, result_list, order_of_hand=0){
+
+    for (let i=0; i<history.length; i++){
+
+        let action = history[i];
+
+        action["parent"] = history;
+        action["order_in_parent"] = i;
+
+        // 全体で何手目かを記録
+
+        // 先頭ではなかったら
+        if (i > 0){
+            // 指し手だったら、数字を１つ増やす
+            if ( Object.keys(action).indexOf("move") >= 0 ){
+                action["order_of_hand"] = history[i-1]["order_of_hand"] + 1;
+            }
+            // 指し手以外だったら、前と同じに
+            else {
+                action["order_of_hand"] = history[i-1]["order_of_hand"];
+            }
+        }
+        // 先頭だったら
+        else {
+            // 指し手だったら、数字を１つ増やす
+            if ( Object.keys(action).indexOf("move") >= 0 ){
+                action["order_of_hand"] = order_of_hand + 1;
+            }
+            // 指し手以外だったら、前と同じに
+            else {
+                action["order_of_hand"] = order_of_hand;
+            }
+        }
+
+        result_list.push(action);
+
+        if (Object.keys(action).indexOf("scenarios") >= 0){
+            let mini_history = action["scenarios"][action["selected_scenario"]];
+            list_showing_actions(mini_history, result_list, order_of_hand=action["order_of_hand"]);
+        }
+    }
+}
+
+
+
+class HistoryHandler{
+
+    constructor(){
+
+        this.history = [];
+
+        let initial_action = {
+            "move" : "initial",
+            "move_str" : "initial",
+            "parent" : this.history,
+            "order_in_parent" : 0,
+            "board_state" : deepcopy_Board(SBoard.Board),
+            "is_watching" : true
+        };
+        this.history.push(initial_action);
+
+        this.watching_action = this.history[0];
+        this.watching_dom = null;
+
+        list_showing_actions(this.history, emp_list);
+
+        update_d3(emp_list);
+
+    }
+
+
+
+    add_action(action, is_fly_to=false){
+
+        // 以前見ていたもの
+        $(History.watching_dom).css("background-color", "white");
+        this.watching_action["is_watching"] = false;
+
+
+        // 新たなブランチを作成する　：　親の最新でないアクションを見ている時にmoveアクションが追加されたら
+        if ((Object.keys(action).indexOf("scenarios") < 0) && (History.watching_action["order_in_parent"] != History.watching_action["parent"].length - 1)){
+
+            this.make_branch(action);
+        }
+
+        // サブシナリオを追加する
+        else if (Object.keys(this.watching_action).indexOf("scenarios") >= 0){
+            this.add_branch(action);
+        }
+
+        else{
+
+            // まず、historyに追加する
+            if (is_fly_to){
+                this.watching_action["parent"].push(action);
+            }
+            else{
+                this.watching_action["parent"].splice(this.watching_action["order_in_parent"]+1, 0, action);
+            }
+        }
+
+        // 見ているものを更新
+        this.watching_action = action;
+        action["is_watching"] = true;
+        
+        this.update_view();
+        show_message_contents(action);
+
+    }
+
+    add_action_before(action){
+
+        // 手前にアクションを追加する
+
+        // 今はメッセージアクションだけ
+        if (Object.keys(action).indexOf("message") < 0){
+            console.log("okasiine");
+            exit;
+        }
+
+        // 以前見ていたもの
+        $(History.watching_dom).css("background-color", "white");
+        this.watching_action["is_watching"] = false;
+
+        // historyに追加
+        this.watching_action["parent"].splice(this.watching_action["order_in_parent"], 0, action);
+
+        // 見ているものを更新
+        this.watching_action = action;
+        action["is_watching"] = true;
+        
+        this.update_view();
+        show_message_contents(action);
+
+    }
+
+    make_branch(action){
+
+        let order_in_parent = this.watching_action["order_in_parent"];
+        let after_watching_action = this.watching_action["parent"].slice(order_in_parent+1);
+        
+
+        // 新しいシナリオアクション
+        let new_scenario_action = {
+            "scenarios" : [
+                after_watching_action, // 今参照しているアクションより後のhistory
+                [action]
+            ],
+            "parent" : this.watching_action["parent"],
+            "order_in_parent" : order_in_parent + 1,
+            "selected_scenario" : 1,
+            "board_state" : deepcopy_Board(this.watching_action["board_state"])
+        };
+
+
+        this.watching_action["parent"].splice(order_in_parent+1, this.watching_action["parent"].length-(order_in_parent+1));
+        this.watching_action["parent"].push(new_scenario_action);  // 作成したシナリオアクションを追加
+
+    }
+
+    add_branch(action){
+
+        // もしすでにブランチが存在していたら、そこに加える
+
+        let new_sc = [action];
+        this.watching_action["scenarios"].push(new_sc);
+        this.watching_action["selected_scenario"] = this.watching_action["scenarios"].length - 1;
+    }
+
+
+    update_view(){
+        emp_list = [];
+        list_showing_actions(this.history, emp_list);
+        update_d3(emp_list);
+    }
+}
+
+
+
+
+function show_message_contents(action){
+
+    let keys = Object.keys(action);
+
+    if (keys.indexOf("message") >= 0){
+        
+        let message_action = action["message"];
+        let message_keys = Object.keys(message_action);
+
+        // テキスト
+        var el = "#TextAction_text_box";
+        if (message_keys.indexOf("text") >= 0){
+            $(el).val(message_action["text"]);
+        }
+        else{
+            $(el).val("");
+        }
+
+        // ライトアップ
+        var el = "#LightUpAction_buttons p";
+        if (message_keys.indexOf("light_up") >= 0){
+            $(el).text(message_action["light_up"].join(","));
+        }
+        else{
+            $(el).text("");
+        }
+
+        // マーク
+        var el = "#MarkAction_buttons p";
+        if (message_keys.indexOf("mark") >= 0){
+            $(el).text(message_action["mark"].join(","));
+        }
+        else{
+            $(el).text("");
+        }
+    }
+
+    else {
+        $("#TextAction_text_box").val("");
+        $("#LightUpAction_buttons p").text("");
+        $("#MarkAction_buttons p").text("");
+    }
+}
+
+
+
+
+// メッセージを見ている時
+
+
+function show_effects(reset_color=true){
+
+    // reset_color : 最初に各マス目の色を元に戻す（以前のアクションによる着色をリセットするため）か、戻さない（マークの追加時など）か
+
+    if (reset_color){
+        $(".OneSquare").css("background-color", SBoard.default_color);
+    }
+
+    let watching_action = History.watching_action;
+
+
+    // メッセージアクションだったら、背景とマークの位置を着色
+
+    if (Object.keys(watching_action).indexOf("message") >= 0){
+
+        let message = watching_action["message"];
+
+        if (Object.keys(message).indexOf("light_up") >= 0){
+            for (let loc_light_up of message["light_up"]){
+                $("#" + loc_light_up).css("background-color", "pink");
+            }
+        }
+
+        if (Object.keys(message).indexOf("mark") >= 0){
+            for (let loc_mark of message["mark"]){
+                $("#" + loc_mark).css("background-color", "lightcyan");
+            }
+        }
+    }
+
+    else if (Object.keys(watching_action).indexOf("move") >= 0){
+        let loc = watching_action["move"][2] + watching_action["move"][3];
+        $("#" + loc).css("background-color", "indianred");
+    }
+}
+
+
+$(document)
+    // .on("mouseenter", ".OneAction", function(){
+    //     let watching_action = History.watching_action;
+    //     if (Object.keys(watching_action).indexOf("message") >= 0){
+    //         let message = watching_action["message"];
+    //         if (Object.keys(message).indexOf("light_up") >= 0){
+    //             for (let loc_light_up of message["light_up"]){
+    //                 $("#" + loc_light_up).css("background-color", "pink");
+    //             }
+    //         }
+    //         if (Object.keys(message).indexOf("mark") >= 0){
+    //             for (let loc_mark of message["mark"]){
+    //                 $("#" + loc_mark).css("background-color", "lightcyan");
+    //             }
+    //         }
+    //     }
+    // })
+    // .on("mouseleave", ".OneAction", function(){
+    //     $(".OneSquare").css("background-color", SBoard.default_color);
+    // })
+    .on("click", ".OneAction", show_effects)
+
+
+
+
+// 常に監視する関数たち
+
+
+// function open_or_close_sub_sc(){
+//     let watching_action = History.watching_action;
+//     if (Object.keys(watching_action).indexOf("scenarios") >= 0){
+//         open_pull();
+//     }
+//     else{
+//         close_pull();
+//     }
+// }
---
+++ board/static/js/ShogiBoard/message.js
@@ -0,0 +1,315 @@
+// メッセージアクションをヒストリーに追加
+
+$(document).on("click", "#AddAction", function(){
+
+    SBoard.init_state_now();
+
+    let text = $("#TextAction_text_box").val();
+    let LightUp_pos_str = $("#" + "LightUpAction_buttons p").text();
+    let Mark_pos_str = $("#" + "MarkAction_buttons p").text();
+
+    let action = {"message" : {}, "board_state" : deepcopy_Board(SBoard.Board)};
+
+    if ((text != null) && (text != "")){
+        action["message"]["text"] = text;
+    }
+    if ((LightUp_pos_str != null) && (LightUp_pos_str != "")){
+        action["message"]["light_up"] = LightUp_pos_str.split(",");
+    }
+    if ((Mark_pos_str != null) && (Mark_pos_str != "")){
+        action["message"]["mark"] = Mark_pos_str.split(",");
+    }
+
+
+    History.add_action(action);
+    show_message_contents(action);
+
+})
+
+
+// 手前に追加
+$(document).on("click", "#AddActionBefore", function(){
+
+    SBoard.init_state_now();
+
+    let text = $("#TextAction_text_box").val();
+    let LightUp_pos_str = $("#" + "LightUpAction_buttons p").text();
+    let Mark_pos_str = $("#" + "MarkAction_buttons p").text();
+
+    // 一個手前の盤面をコピー
+    let order = History.watching_action["order_in_parent"];
+    if (order > 0){
+        var board_state_before = History.watching_action["parent"][order-1]["board_state"];
+    }
+    else {
+        window.alert("シナリオの直後はだめ！（ただいま実装中）");
+        return
+    }
+
+    // let action = {"message" : {}, "board_state" : deepcopy_Board(SBoard.Board)};
+    let action = {"message" : {}, "board_state" : deepcopy_Board(board_state_before)};
+
+    if ((text != null) && (text != "")){
+        action["message"]["text"] = text;
+    }
+    if ((LightUp_pos_str != null) && (LightUp_pos_str != "")){
+        action["message"]["light_up"] = LightUp_pos_str.split(",");
+    }
+    if ((Mark_pos_str != null) && (Mark_pos_str != "")){
+        action["message"]["mark"] = Mark_pos_str.split(",");
+    }
+
+
+    History.add_action_before(action);
+    show_message_contents(action);
+
+})
+
+
+// メッセージアクションの内容を更新
+
+$(document).on("click", "#UpdateAction", function(){
+
+    let action = History.watching_action;
+
+    // 現在参照しているのがメッセージアクションかチェック
+    if (Object.keys(action).indexOf("message") < 0){
+        window.alert("メッセージアクションにのみ適用できます");
+        exit;
+    }
+
+    let text = $("#TextAction_text_box").val();
+    let LightUp_pos_str = $("#" + "LightUpAction_buttons p").text();
+    let Mark_pos_str = $("#" + "MarkAction_buttons p").text();
+
+    if ((text != null) && (text != "")){
+        action["message"]["text"] = text;
+    }
+    else{
+        delete action["message"]["text"];
+    }
+
+    if ((LightUp_pos_str != null) && (LightUp_pos_str != "")){
+        action["message"]["light_up"] = LightUp_pos_str.split(",");
+    }
+    else{
+        delete action["message"]["light_up"];
+    }
+
+    if ((Mark_pos_str != null) && (Mark_pos_str != "")){
+        action["message"]["mark"] = Mark_pos_str.split(",");
+    }
+    else{
+        delete action["message"]["mark"];
+    }
+
+    action["is_watching"] = true;
+    History.update_view();
+    show_message_contents(action);
+    
+})
+
+
+
+// fly_toアクションを追加
+
+$(document).on("click", "#add_FlyTo", function(){
+
+    // 現在参照しているのがmessageだったらエラーを出す
+    if (Object.keys(History.watching_action).indexOf("message") > 0){
+        window.alert("メッセージのところにfly_toは追加できません");
+        exit;
+    }
+
+    let action = {"fly_to" : null, "board_state" : deepcopy_Board(SBoard.Board)};
+    History.add_action(action, true);
+})
+
+
+
+// アクションを削除
+$(document).on("click", "#DelAction", function(){
+
+    SBoard.init_state_now();
+
+    let order = History.watching_action["order_in_parent"];
+
+    // サブシナリオの最初を削除したら
+    if (order == 0){
+
+        // initialは削除できない
+        if (Object.keys(History.watching_action).indexOf("move") >= 0){
+            if (History.watching_action["move"] == "initial"){
+                window.alert("最初は削除できません");
+                exit;
+            }
+        }
+
+        // 見ているもの以降を削除
+        History.watching_action["parent"].splice(order);
+        History.history = handle_emp_scenario(History.history);
+
+       // 画面更新
+       History.update_view();
+       show_message_contents(History.watching_action);
+       SBoard.Board = deepcopy_Board(History.watching_action["board_state"]);
+       SBoard.draw_main_board();
+
+    }
+
+    else{
+        // 見ているものを更新
+        History.watching_action = History.watching_action["parent"][order-1];
+        History.watching_action["is_watching"] = true;
+
+        // 見ているもの以降を削除
+        History.watching_action["parent"].splice(order);
+        
+        // 画面更新
+        History.update_view();
+        show_message_contents(History.watching_action);
+        SBoard.Board = deepcopy_Board(History.watching_action["board_state"]);
+        SBoard.draw_main_board();
+    }
+    
+})
+
+
+// function handle_emp_scenario(history){
+
+//     // ブランチを削除した時（ブランチの最初のアクションを削除した時）に、historyを編集
+
+//     for (let i=0; i<history.length; i++){
+//         let action = history[i];
+
+//         // シナリオアクションを見つけたら
+//         if (Object.keys(action).indexOf("scenarios") >= 0){
+//             let emp_sc_number = null;
+//             let n_sub_sc = action["scenarios"].length;
+
+//             for (let j=0; j<n_sub_sc; j++){
+//                 sub_sc = action["scenarios"][j];
+
+//                 // 空のシナリオだったら
+//                 if (sub_sc.length == 0){
+//                     emp_sc_number = j;
+//                     break;
+//                 }
+//                 // 空じゃなかったら、そのシナリオの中身を同じようにチェック
+//                 else{
+//                     action["scenarios"][j] = handle_emp_scenario(sub_sc);
+//                 }
+//             }
+//             // 空のシナリオを見つけていたら
+//             if (emp_sc_number != null){
+//                 if (n_sub_sc >= 3){
+//                     action["scenarios"].splice(emp_sc_number, 1);
+//                 }
+//                 else{
+//                     let survive_sub_sc_number = Math.abs(action["selected_scenario"] - 1);  // 現在見ているのと別の方のシナリオ
+//                     if (survive_sub_sc_number > 1){
+//                         window.alert("koara");
+//                         exit;
+//                     }
+//                     let tail_actions = action["scenarios"][survive_sub_sc_number];
+//                     for (let tail_action of tail_actions){
+//                         tail_action["parent"] = history;
+//                     }
+
+//                     history.splice(-1);  // ブランチアクションを削除（親の最後)
+//                     history = history.concat(tail_actions); // 確認　見ていない方のサブシナリオを親の最後に加える
+
+//                     History.watching_action = history[i-1];
+//                     History.watching_action["is_watching"] = true;
+//                 }
+//             }
+//         }
+//     }
+
+//     return history;
+// }
+
+
+function handle_emp_scenario(history){
+
+    // ブランチを削除した時（ブランチの最初のアクションを削除した時）に、historyを編集
+
+    for (let i=0; i<history.length; i++){
+        let action = history[i];
+
+        // シナリオアクションだったら
+        if (Object.keys(action).indexOf("scenarios") >= 0){
+
+            let n_sub_sc = action["scenarios"].length;
+
+            let selected_scenario = action["selected_scenario"];
+            let watching_sub_scenario = action["scenarios"][selected_scenario];
+
+            // 空じゃなかったら、そのシナリオの中身を同じようにチェック
+            if (watching_sub_scenario.length >= 1){
+                action["scenarios"][selected_scenario] = handle_emp_scenario(watching_sub_scenario);
+            }
+            // 空のシナリオだったら
+            else{
+                // 分岐が３つ以上なら、見ている分岐を削除するだけ
+                if (n_sub_sc >= 3){
+                    action["scenarios"].splice(selected_scenario, 1);
+                    action["selected_scenario"] = 0;
+                }
+                // 分岐が２つなら、分岐をなくして連結
+                else if (n_sub_sc == 2){
+
+                    action["scenarios"].splice(selected_scenario, 1);  // 今見ている方の分岐を削除
+
+                    // 連結するアクションたち
+                    let tail_actions = action["scenarios"][0];
+                    for (let tail_action of tail_actions){
+                        tail_action["parent"] = history;
+                    }
+
+                    history.splice(i, 1);  // 分岐アクション自体を削除
+                    history = history.concat(tail_actions); // 見ていない方のサブシナリオを親の最後に加える
+
+                    History.watching_action = history[i-1];
+                    History.watching_action["is_watching"] = true;
+
+                }
+                // 分岐は２つ以上あるはず
+                else {
+                    window.alert("バグです。「分岐は１つだけのはず」と作成者に報告してください");
+                }
+            }
+        }
+    }
+
+    console.log("this is result");
+    console.log(history);
+
+    return history;
+}
+
+
+
+
+// メッセージエフェクトを足すボタンを押した時
+$(document).on("click", ".one_action_add_button", function(){
+    SBoard.now_is_adding_action = !SBoard.now_is_adding_action;
+    if (SBoard.now_is_adding_action){
+        $(".OneSquare").css("background-color", "gold");
+        show_effects(false);
+        let adding_action_kind = this.id.split("_")[1];
+        SBoard.now_is_adding_kind = adding_action_kind;
+    }
+    else{
+        // $(".OneSquare").css("background-color", SBoard.default_color);
+        show_effects();
+    }
+})
+
+
+// メッセージエフェクトを消すボタンを押した時
+$(document).on("click", ".one_action_reflesh_button", function(){
+    let adding_action_kind = this.id.split("_")[1]; // "LightUp" or "Mark"
+    $("#" + adding_action_kind + "Action_buttons" + " p").text("");
+})
+
