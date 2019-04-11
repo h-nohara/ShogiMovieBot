0aa6bbbed9e94a339f6a1e813da4f8f0f9b1499b
Nohara <selaphymsigoto@gmail.com>
2019-04-11 09:54:27 +0900

    chapters page

--- ShareShogi/views.py
+++ ShareShogi/views.py
@@ -10,7 +10,7 @@ def search_page(request):
     return render(request, "ShareShogi/search_book.html")
 
 def chapters_page(request):
-    return render(request, "ShareShogi/chapters_test.html")
+    return render(request, "ShareShogi/chapters.html")
 
 def scene_page(request):
     return render(request, "ShareShogi/scenes.html")
